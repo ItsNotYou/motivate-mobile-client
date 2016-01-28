334ca5cd640f46b18c2b45c016e4544c5b9f28ee
Kristin Sass <ksass@uni-potsdam.de>
2016-01-28 11:39:39 +0100

    added WifiEnabledWidget

---
+++ www/scripts/motivate_adaptation_engine/context/widgets/WifiEnabledWidget.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Kristin on 25.01.2016.
+ */
+define(['contactJS', './WidgetCreator'], function (contactJS, creator) {
+    return (function() {
+        return creator.extend("WifiEnabledWidget", {
+            description : {
+                out: [
+                    {
+                        'name': 'CI_WIFI_ENABLED',
+                        'type': 'BOOLEAN',
+                        'parameterList': [["CP_UNIT", "STRING", "WIFI_ENABLED"]]
+                    }
+                ],
+                updateInterval: 5000
+            },
+            simpleQueryGenerator: function(callback) {
+                var result = "false";
+                WifiWizard.isWifiEnabled(function(result){});
+                //console.log("Tini: "+result);
+                callback({0: result});
+            }
+        });
+    })();
+});
