6778fb4ea2f402902ccc70b93a1e469806b18b0e
Kristin Sass <ksass@uni-potsdam.de>
2016-04-05 01:55:30 +0200

    modified TrovisConnectedWidget: next try - error handling

--- www/scripts/motivate_adaptation_engine/context/widgets/TrovisConnectedWidget.js
+++ www/scripts/motivate_adaptation_engine/context/widgets/TrovisConnectedWidget.js
@@ -33,8 +33,31 @@ define(['contactJS', 'jquery', './WidgetCreator'], function(contactJS, $, Widget
 
             };
 
-            var error = function (xhr, ajaxOptions, thrownError) {
-                alert(request.responseText);
+            var error = function(e, x, settings, exception) {
+                var message;
+                var statusErrorMap = {
+                    '400' : "Server understood the request, but request content was invalid.",
+                    '401' : "Unauthorized access.",
+                    '403' : "Forbidden resource can't be accessed.",
+                    '500' : "Internal server error.",
+                    '503' : "Service unavailable."
+                };
+                if (x.status) {
+                    message =statusErrorMap[x.status];
+                    if(!message){
+                        message="Unknown Error \n.";
+                    }
+                }else if(exception=='parsererror'){
+                    message="Error.\nParsing JSON Request failed.";
+                }else if(exception=='timeout'){
+                    message="Request Time out.";
+                }else if(exception=='abort'){
+                    message="Request was aborted by the server";
+                }else {
+                    message="Unknown Error \n.";
+                }
+                $(this).css("display","inline");
+                $(this).html(message);
             };
 
 
