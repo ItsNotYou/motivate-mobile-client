b2657ed68c3033a5a7533863b9b76aad70e21094
Kristin Sass <ksass@uni-potsdam.de>
2016-04-13 23:01:48 +0200

    try to compare time with time range

--- www/scripts/motivate_adaptation_engine/context/interpreters/TimeInterpreter.js
+++ www/scripts/motivate_adaptation_engine/context/interpreters/TimeInterpreter.js
@@ -25,14 +25,22 @@ define(['contactJS', './InterpreterCreator'], function (contactJS, creator) {
             simpleInterpretData: function(values, callback) {
                 //trovis time in format hh:mm
                 var t_time = values[0].time;
-                var d = new Date();
+                var date = new Date();
                 //device time in format hh:mm
-                var d_time = d.getHours()+":"+d.getMinutes()
+                var m_time = date.getHours()+":"+date.getMinutes();
 
-                //compare date and device time
-                //Sommer-/Winderzeit!!!
+                var m1_time = date.getHours()+":"+(date.getMinutes()-2);
+                var m2_time = date.getHours()+":"+(date.getMinutes()+2);
 
-                if (d_time == t_time){
+                var hours = t_time.getHours();
+                var minutes = t_time.getMinutes();
+
+                //alert(m1_time + ", " + m2_time + "\n" + hours + ", " + minutes);
+                alert(m2_time);
+
+                //Sommer-/Winterzeit!!!
+
+                if (t_time == m_time){
                     callback({0: "true"});
                 } else {
                     callback({0: "false"});
